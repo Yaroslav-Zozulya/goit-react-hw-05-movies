e898ec2e85dce0103e1f25e5df9aedcc1cec26c8
Yaroslav Zozulya <my.dev.stories@gmail.com>
2022-07-26 15:04:52 +0300

    init project

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+export const App = () => {
+  console.log('hello world');
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      React homework template
+    </div>
+  );
+};
