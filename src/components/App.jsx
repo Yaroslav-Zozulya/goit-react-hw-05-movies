06640bb173fdf6c075f41717588be2541123f22b
Yaroslav Zozulya <my.dev.stories@gmail.com>
2022-07-28 15:58:46 +0300

    check basename

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
-import Layout from './Layout/Layout';
-import Home from './Home/Home';
-import MovieDetails from './MovieDetails/MovieDetails';
+import { lazy, Suspense } from 'react';
+// import Layout from './Layout/Layout';
+// import Home from './Home/Home';
+// import MovieDetails from './MovieDetails/MovieDetails';
+import Loader from './Loader/Loader';
+
+const Home = lazy(() => import('./Home/Home'));
+const Layout = lazy(() => import('./Layout/Layout'));
+const MovieDetails = lazy(() => import('./MovieDetails/MovieDetails'));
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="movies" element={<div>movies</div>} />
-        <Route path="movies/:itemId" element={<MovieDetails />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="movies" element={<div>movies</div>} />
+          <Route path="movies/:itemId" element={<MovieDetails />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,10 +1,16 @@
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { NavLink, useParams } from 'react-router-dom';
+import styled from 'styled-components';
 import Loader from 'components/Loader/Loader';
 import { getMovieDetails } from 'services/moviesApi';
 
+const Link = styled(NavLink)`
+  display: flex;
+  gap: 10px;
+`;
+
 const MovieDetails = () => {
-  const [items, setItems] = useState([]);
+  const [item, setItem] = useState([]);
   const [loading, setLoading] = useState(false);
   const { itemId } = useParams();
 
@@ -12,8 +18,8 @@ const MovieDetails = () => {
     const fetchTrendingMovies = async () => {
       setLoading(true);
       try {
-        const itemsData = await getMovieDetails(itemId);
-        setItems(itemsData);
+        const itemData = await getMovieDetails(itemId);
+        setItem(itemData);
       } catch (error) {
         console.log(error);
       } finally {
@@ -22,11 +28,42 @@ const MovieDetails = () => {
     };
     fetchTrendingMovies();
   }, [itemId]);
-  console.log(items);
+  console.log(item);
+  const { title, overview, poster_path, release_date } = item;
+
   return (
     <>
       {loading && <Loader />}
-      <div>movies details</div>
+      <div>
+        <img
+          src={
+            poster_path
+              ? `https://image.tmdb.org/t/p/w500${poster_path}`
+              : 'fuck you'
+          }
+          alt=""
+        />
+
+        <h1>
+          {title} ({release_date && release_date.slice(0, 4)})
+        </h1>
+        <p></p>
+        <h2>Overview</h2>
+        <p>{overview}</p>
+        <h2>Genres</h2>
+        <p>
+          {item.genres.length > 0 ? (
+            <p>{item.genres.map(genre => genre.name).join(', ')}</p>
+          ) : (
+            'not found'
+          )}
+        </p>
+        <hr />
+        <p>Additional information</p>
+        <Link to="">Cast</Link>
+        <Link to="">Reviews</Link>
+        <hr />
+      </div>
     </>
   );
 };
